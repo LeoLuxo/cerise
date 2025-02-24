ed60ba6ed143a43d6b0a3572c77419be290aa230
LeoLuxo <contact@me.leoluxo.eu>
2025-02-24 09:43:16 +0100

    Add flake build

--- flake.nix
+++ flake.nix
@@ -2,36 +2,69 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    opam-nix.url = "github:tweag/opam-nix";
+
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-coq = {
+      url = "github:coq/opam";
+      flake = false;
+    };
   };
 
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      opam-nix,
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+      opam-repository,
+      opam-coq,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) coqPackages;
+        on = opam-nix.lib.${system};
+
+        name = "coq-cerise";
+
+        repos = [
+          "${opam-repository}"
+          "${opam-coq}/released" # Contains iris, equations and stdpp
+          "${opam-coq}/extra-dev" # Contains coq-lsp and vscoq
+        ];
+
+        # Extra dependencies to add to the project; the ones from the .opam file will be loaded automatically
+        extraQuery = {
+          ocaml-base-compiler = "4.14.1"; # Required for version 8.18 of coq
+          coq-lsp = "*";
+          vscoq-language-server = "*";
+        };
+
+        # Build the opam project
+        project = on.buildOpamProject' { inherit pkgs repos; } ./. extraQuery;
       in
-        rec {
-          packages.default = coqPackages.mkCoqDerivation {
-              pname = "cerise";
-              version = "0.0.0";
+      {
 
-              coq-version = "8.20";
+        devShells.default = pkgs.mkShell (
+          let
+            inherit (project.${name}) pname buildInputs;
+          in
+          {
+            name = pname + "-dev";
+            packages = buildInputs ++ [
+              pkgs.gnumake
 
-              src = ./.;
-
-              buildInputs = with coqPackages; [ equations iris stdpp ];
-
-              meta = with pkgs.lib; {
-                description = "Cerise, Coq mechanization of a capability machine and principles to reason about the interaction of known and unknown code";
-                homepage = "https://github.com/logsem/cerise";
-                license = licenses.bsd3;
-              };
-            };
-
-            devShells.default = pkgs.mkShell (with packages.default; {
-              name = pname + "-dev";
-              packages = buildInputs ++ [ pkgs.coq coqPackages.coq-lsp ];
-            });
-          });
+              # Add both LSPs, use whichever is preferred
+              project.coq-lsp
+              project.vscoq-language-server
+            ];
+          }
+        );
+      }
+    );
 }
