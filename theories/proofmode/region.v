9c72fd3d2ca6f346bc359bc10ebd9b1139293c00
LeoLuxo <contact@me.leoluxo.eu>
2025-04-03 08:59:08 +0200

    Delete all binary

--- theories/proofmode/region.v
+++ theories/proofmode/region.v
@@ -1,5 +1,5 @@
 From cap_machine Require Export stdpp_extra cap_lang rules_base.
-From cap_machine Require Import rules_binary_base.
+(* From cap_machine Require Import rules_binary_base. *)
 From iris.proofmode Require Import tactics.
 From machine_utils Require Import finz_interval.
 From cap_machine Require Import addr_reg. (* Required because of a weird Coq bug related to imports *)
